e9df5f44c675a7cb6dd281d92ed3432294748a57
Wolfffff <scott.w.wolf1@gmail.com>
2024-03-04 13:15:13 -0500

    Updating demultiplex

--- scripts/demultiplex.sh
+++ scripts/demultiplex.sh
@@ -2,7 +2,7 @@
 
 
 #Make tsv with sample names in first column and two barcodes joined by a hyphen (i7-i5) in second, for demultiplexing
-awk '{print $1 "\t" $2 "-" $3}' < barcodes_all.tsv > barcodes_fmtd.fil
+awk '{print $1 "\t" $9 "-" $12}' < /Genomics/ayroleslab2/shared/genomics-in-the-wild-2024/data/giw2024/metadata.txt > barcodes_fmtd.fil
 
 #Get fastq-multx from github
 git clone https://github.com/brwnj/fastq-multx
@@ -10,12 +10,10 @@ cd fastq-multx
 make
 
 #Run the demultiplexing (on files in 01_raw_fastqs)
-./fastq-multx -B barcodes_fmtd.fil \
--m 1 \
-INFO-000000000-DHCH6_1_Read_2_Index_Read_passed_filter.fastq.gz \
-INFO-000000000-DHCH6_1_Read_3_Index_Read_passed_filter.fastq.gz \
-INFO-000000000-DHCH6_1_Read_1_passed_filter.fastq.gz \
-INFO-000000000-DHCH6_1_Read_4_passed_filter.fastq.gz \
+fastq_demux -B /Genomics/ayroleslab2/shared/genomics-in-the-wild-2024/data/giw2024/metadata.txt \
+-m 0 \
+--R1 /Genomics/ayroleslab2/shared/genomics-in-the-wild-2024/data/giw2024/J1_S1_L001_R1_001.fastq.gz \
+--R2/Genomics/ayroleslab2/shared/genomics-in-the-wild-2024/data/giw2024/J1_S1_L001_R2_001.fastq.gz \
 -o n/a \
 -o n/a \
 -o 02_demultiplexed_fastqs/%_R1.fastq \
